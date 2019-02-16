c4dca153485d53956a6d7700845ee0addcaf3b01
juanga16 <juan.desposito@gmail.com>
2019-02-15 21:06:27 -0300

    Update form_get.php
    Formulario GET

--- PHP/Unidad 10/form_get/form_get.php
+++ PHP/Unidad 10/form_get/form_get.php
@@ -10,32 +10,42 @@
         return $parametro;
     }
 
-    $tipoDeNoticiaSeleccionada = "";
-    $tiposDeNoticias = array(1 => "Economia", 2 => "Politica", 3 => "Sociedad", 4 => "Deportes");
-    $termino = "";
+    $idTipoDeNoticia = 0;
+    $tiposDeNoticias = array(1 => "Economia", 2 => "Politica", 3 => "Sociedad", 4 => "Deportes", 5 => "Espectaculos", 6 => "Tecnologia", 7 => "Mundo", 8 => "Arquitectura");
+    $termino = "";    
 
-    if ($_SERVER["REQUEST_METHOD"] == "GET") {
-        if (isset($_GET["tipoDeNoticia"])) {
-            $tipoDeNoticiaSeleccionada = leerEntrada($_GET["tipoDeNoticia"]);
-        }
+    // isset se fija si existe una clave 'idTipoDeNoticia' en el array $_GET
+    if (isset($_GET["idTipoDeNoticia"])) {
+        // Lo convierto a entero anteponiendo (int)
+        $idTipoDeNoticia = (int) $_GET["idTipoDeNoticia"];     
+    }
 
-        if (isset($_GET["termino"])) {
-            $termino = leerEntrada($_GET["termino"]);
-        }    
+    // isset se fija si existe una clave 'termino' en el array $_GET
+    if (isset($_GET["termino"])) {
+        $termino = $_GET["termino"];
     }
 ?>
 
 <html>
 <body>
+    <!-- PHP_SELF le decimos que postee el formulario a si mismo -->
     <form method="get" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
         <label>Tipo de Noticia</label>
         </br>
 
-        <select name="tipoDeNoticia" required>
+        <select name="idTipoDeNoticia" required>
             <option value=""></option>
             <?php
                 foreach ($tiposDeNoticias as $clave => $valor) {
-                    $seleccionado = ($clave == $tipoDeNoticiaSeleccionada ? "selected" : "");
+                    /*  esto se conoce como iif, es lo mismo que
+                        if ($clave == $idTipoDeNoticia) {
+                            $seleccionado = "selected"
+                        } else {}
+                            $seleccionado = ""
+                        }
+                    */
+
+                    $seleccionado = ($clave == $idTipoDeNoticia ? "selected" : "");
                     echo "<option value='$clave' $seleccionado>$valor</option>";            
                 }            
             ?>
@@ -47,10 +57,13 @@
         </br>
         <input type="submit" value="Buscar">
 
-        <?php
-            if ($_SERVER["REQUEST_METHOD"] == "GET") {
-                echo "<h3>Tipo de Noticia: $tiposDeNoticias[$tipoDeNoticiaSeleccionada]</h3>";
-                echo "<h3>Termino: $termino</h3>";        
+        <?php            
+            if ($idTipoDeNoticia > 0) {
+                echo "<h1>Tipo de Noticia: $tiposDeNoticias[$idTipoDeNoticia]</h1>";
+            }
+
+            if (! empty($termino)) {
+                echo "<h1>Termino: $termino</h1>";
             }
         ?>
     </form>
