e45e3da5a50c6536c164adf70a1da38a053493cc
Juan Gabriel D'Esposito <jdesposito@itx.com>
2019-02-21 22:26:21 -0300

    Ejemplos MySql usando PDO

--- Codigo Fuente/Unidad 14/include/abrir_conexion.php
+++ Codigo Fuente/Unidad 14/include/abrir_conexion.php
@@ -1,5 +1,6 @@
 <?php
     // Comparar APIs: http://php.net/manual/es/mysqlinfo.api.choosing.php
+    // Estos ejemplos estan basados en PDO que significa PHP Data Object
 
     $servidor = "localhost:3306";
     $usuario = "root";
