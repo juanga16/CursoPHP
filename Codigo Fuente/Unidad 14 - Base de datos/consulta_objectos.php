d6ef77be1c595377852c0de67a90a5e63cb1dee2
Juan Gabriel D'Esposito <jdesposito@itx.com>
2019-02-27 18:12:45 -0300

    Objetos

---
+++ Codigo Fuente/Unidad 14 - Base de datos/consulta_objectos.php
@@ -0,0 +1,109 @@
+<?php
+    require "include/abrir_conexion.php";
+?>
+
+<h1>Consultas a la base de datos</h1>
+
+<?php
+    class Pelicula {   
+        // Variables
+        private $id;
+        private $titulo;
+        private $calificacion;
+        private $nacionalidad;
+        private $poster;
+        private $fechaEstreno;
+        
+        //Setters
+        public function setId($id){
+            $this->id = $id;
+        }
+        
+        public function setTitulo($titulo){
+            $this->titulo = $titulo;
+        }
+        
+        public function setCalificacion($calificacion){
+            $this->calificacion = $calificacion;
+        }
+        
+        public function setNacionalidad($nacionalidad){
+            $this->nacionalidad = $nacionalidad;
+        }
+        
+        public function setPoster($poster){
+            $this->poster = $poster;
+        }
+        
+        public function setFechaEstreno($fechaEstreno){
+            $this->fechaEstreno = $fechaEstreno;
+        }
+
+        //Getters
+        public function getId(){
+            return $this->id;
+        }
+        
+        public function getTitulo(){
+            return $this->titulo;
+        }
+        
+        public function getCalificacion(){
+            return $this->calificacion;
+        }
+        
+        public function getNacionalidad(){
+            return $this->nacionalidad;
+        }
+
+        public function getPoster(){
+            return $this->poster;
+        }
+
+        public function getFechaEstreno(){
+            return $this->fechaEstreno;
+        }        
+    }
+
+    // Armo la consulta
+    $consulta = $conexion->prepare("SELECT id, titulo, calificacion, nacionalidad, poster, fecha_estreno as fechaEstreno FROM pelicula ORDER BY calificacion");
+    $consulta->execute();
+    $resultado = $consulta->fetchAll(PDO::FETCH_CLASS, 'Pelicula');
+
+    // Para obtener la cantidad de registros
+    echo "<h2>Cantidad de registros: ".$consulta->rowCount()."</h2>";
+
+    if ($resultado) {
+        echo "<table>";
+        echo "<tr>";
+        echo "<th>Id</th>";
+        echo "<th>Titulo</th>";
+        echo "<th>Fecha de Estreno</th>";
+        echo "<th>Calificacion</th>";
+        echo "<th>Nacionalidad</th>";
+        echo "<th>Poster</th>";
+        echo "</tr>";
+
+        foreach ($resultado as $registro) {
+            $fechaEstreno = new DateTime($registro->getFechaEstreno());
+
+            // Imprimimos cada fila
+            echo "<tr>";
+            echo "<td>".$registro->getId()."<td/>";
+            echo "<td>".utf8_encode($registro->getTitulo())."<td/>";            
+            echo "<td>".$fechaEstreno->format('d/m/Y H:i')."<td/>";
+            echo "<td>".$registro->getCalificacion()."<td/>";
+            echo "<td>".$registro->getNacionalidad()."<td/>";
+            echo "<td><img width='50px' src='posters/".$registro->getPoster()."'/></td>";
+            echo "</tr>";
+
+            // utf8_encode: sirve para imprimir las ñ correctamente
+        }
+
+        echo "</table>";
+    }
+
+    $consulta = null;
+    $resultado = null;
+    require "include/cerrar_conexion.php";
+?>
---
+++ Codigo Fuente/Unidad 15 - Clases/objetos.php
@@ -0,0 +1,97 @@
+<?php
+	abstract class Deportista {
+		// Variables
+		protected $nombre;
+		protected $numero;
+		
+		// Puedo declarar tan solo 1 constructor y pierdo el constructor sin parametros
+		public function __construct($nombre, $numero) {
+			$this->nombre = $nombre;
+			$this->numero = $numero;
+
+			echo "En el constructor Deportista";
+			echo "<br/>";
+		}
+
+		function __destruct() {
+			echo "Destruyendo $this->nombre";
+			echo "<br/>";
+		}
+		
+		// Setters
+		public function setNombre($nombre) {
+			$this->nombre = $nombre;
+		}
+		
+		public function setNumero($numero) {
+			$this->numero = $numero;
+		}
+		
+		// Getters
+		public function getNombre() {
+			return $this->nombre;
+		}
+		
+		public function getNumero() {
+			return $this->numero;
+		}
+
+		// Metodo estatico
+		public static function imprimirTitulo() {
+			echo "<h1>Ejemplo de herencia</h1>";
+		}
+	}
+
+	interface iBasquetbolista {
+		public function lanzarTriple();
+	}
+
+	final class Basquetbolista extends Deportista implements iBasquetbolista {
+		// Variables
+		private $altura;
+
+		function __construct($nombre, $numero, $altura) {
+			parent::__construct($nombre, $numero);
+			$this->altura = $altura;
+
+			echo "En el constructor Basquetbolista";
+			echo "<br/>";
+		}
+
+		public static function obtener($nombre, $numero) {
+			$instancia = new self($nombre, $numero, 200);
+
+			return $instancia;
+		}
+
+		public function setAltura($altura){
+			$this->altura = $altura;
+		}
+
+		public function getAltura(){
+			return $this->altura;
+		}
+
+		// Metodos
+		public function presentar() {
+			echo "<h3>Hola me llamo ".$this->nombre.", mi numero de camiseta es ".$this->getNumero()." y mido ".$this->getAltura()." metros</h3>";
+		}
+
+		public function lanzarTriple() {
+			echo "<h3>".$this->nombre." esta lanzando de 3!</h3>";
+		}
+	}
+
+	Deportista::imprimirTitulo();
+
+	$manuGinobili = new Basquetbolista("Emanuel Ginobili", 20, 1.98);
+	$manuGinobili->presentar();
+	$manuGinobili->setNombre("Manu");
+	$manuGinobili->presentar();
+	$manuGinobili->lanzarTriple();
+	$manuGinobili = null;
+
+	$andresNocioni = Basquetbolista::obtener("Andres Nocioni", 5);
+	$andresNocioni->presentar();
+	$andresNocioni = null;
+?>
